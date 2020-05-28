a9760956e8180f2d02734719581a0b0049b55f2a
Robert Hülsmann <robert@huelsi.de>
2020-05-28 22:21:47 +0200

    working on map

--- map.py
+++ map.py
@@ -5,21 +5,21 @@ import matplotlib.patches as patches
 
 
 class Map:
-    def __init__(self, size_x, size_y, nr_player=1, obstacle_map=None, next_step=False):
+    def __init__(self, size_x, size_y, agent_count=1, obstacle_map=None, next_step=False):
         self._size_x = size_x
         self._size_y = size_y
-        self._nr_player = nr_player
+        self._agent_count = agent_count
         self._next_step = next_step
 
         # Check input values
-        if nr_player < 1:
+        if agent_count < 1:
             raise ValueError('number of agents must be at least one')
-        if size_x * size_y < 2 * nr_player:
+        if size_x * size_y < 2 * agent_count:
             raise ValueError('board size is to small')
 
         # Create layer array of type char to filter the map
         self._layers = np.array([0, 'o'], dtype=np.dtype('U1'))  # obstacles
-        for p in range(nr_player):
+        for p in range(agent_count):
             unstacked_layers = [self._layers,
                                 np.array([p + 1, 'a'], dtype=np.dtype('U1')),  # aim position
                                 np.array([p + 1, 'c'], dtype=np.dtype('U1'))]  # current position
@@ -28,7 +28,7 @@ class Map:
             self._layers = np.vstack(unstacked_layers)
 
         # Create map skeleton
-        nr_layers = 1 + nr_player * (3 if next_step else 2)
+        nr_layers = 1 + agent_count * (3 if next_step else 2)
         self._map = np.zeros((nr_layers, size_x, size_y), dtype=bool)
 
         # Add obstacles to map
@@ -65,34 +65,34 @@ class Map:
         """
         Define [aim, current, next] positions for all agents
         :param layer: 'a' for aim positions, 'c' for current positions or 'n' for next positions, None for all
-        :param positions: (boolean) numpy array with shape [nr_agents * 2] and x and y coordinates inside for all agents
+        :param positions: (boolean) numpy array with shape [agent_count * 2] and x & y coordinates inside for all agents
         """
         if layer == 'n' and not self._next_step:
             raise Exception('Next position not activated in this map')
 
-        positions = np.concatenate([np.expand_dims(np.arange(self._nr_player), 1), positions], 1)
-        ary = np.zeros((self._nr_player, self._size_x, self._size_y))
-        ary[positions[:, 0], positions[:, 1], positions[:, 2]] = [True for _ in range(self._nr_player)]
+        positions = np.concatenate([np.expand_dims(np.arange(self._agent_count), 1), positions], 1)
+        ary = np.zeros((self._agent_count, self._size_x, self._size_y))
+        ary[positions[:, 0], positions[:, 1], positions[:, 2]] = [True for _ in range(self._agent_count)]
         self._map[self._layer_filter(layer=layer)] = ary
 
     def set_aim_positions(self, positions):
         """
         Define aim positions for all agents
-        :param positions: (boolean) numpy array with shape [nr_agents * 2] and x and y coordinates inside for all agents
+        :param positions: (boolean) numpy array with shape [agent_count * 2] and x & y coordinates inside for all agents
         """
         self.set_positions(layer='a', positions=positions)
 
     def set_current_positions(self, positions):
         """
         Define current positions for all agents
-        :param positions: (boolean) numpy array with shape [nr_agents * 2] and x and y coordinates inside for all agents
+        :param positions: (boolean) numpy array with shape [agent_count * 2] and x & y coordinates inside for all agents
         """
         self.set_positions(layer='c', positions=positions)
 
     def set_next_positions(self, positions):
         """
         Define next positions for all agents
-        :param positions: (boolean) numpy array with shape [nr_agents * 2] and x and y coordinates inside for all agents
+        :param positions: (boolean) numpy array with shape [agent_count * 2] and x & y coordinates inside for all agents
         """
         self.set_positions(layer='n', positions=positions)
 
@@ -102,10 +102,10 @@ class Map:
     def get_filtered_map(self, agent=None, layer=None):
         return copy.deepcopy(self._map[self._layer_filter(agent, layer)])
 
-    def get_map_for_agent(self, agent, view_filed):
+    def get_map_for_agent(self, agent, view_filed=None):
         obstacles = self._map[self._layer_filter(layer='o')]
         a_c_n_pos = self._map[self._layer_filter(agent=agent)]
-        other_pos = self._map[self._layer_filter(layer='c')]
+        other_pos = np.any(self._map[self._layer_filter(layer='c')], axis=0)
         other_pos = other_pos & ~self._map[self._layer_filter(agent=agent, layer='c')]  # subtract own current position
         # TODO: field of view
         # other_pos = other_pos & ...
@@ -132,6 +132,13 @@ class Map:
         print(obstacles)
         draw_layer(ax, obstacles, 'black')
 
+        # Agents fields
+        # TODO: continue
+        # colormap = ...
+        # for agent in range(self._agent_count)
+        #     color = ...
+        #     ...
+
         plt.ylim(0, self._size_x)
         plt.xlim(0, self._size_y)
         plt.show()
@@ -146,18 +153,12 @@ if __name__ == '__main__':
                   [0, 0, 0, 0, 0, 0]])
     game_map = Map(6, 6, 2, o)
 
-    # game_map.set_position(agent=0, layer='a', x=5, y=5)
-    # game_map.set_position(agent=1, layer='a', x=3, y=5)
-    # game_map.set_position(agent=0, layer='c', x=0, y=0)
-    # game_map.set_position(agent=1, layer='c', x=0, y=5)
-
     game_map.set_aim_positions([[5, 5],
                                 [3, 5]])
-
     game_map.set_current_positions([[0, 0],
                                     [0, 5]])
 
-    print(game_map.get_map())
+    # print(game_map.get_map())
+    # print(game_map.get_map_for_agent(0))
     # print(game_map.get_filtered_map(agent='2', layer='a'))
-    # game_map.plot_overview()
-    # game_map.get_map_for_agent(0)
+    game_map.plot_overview()
