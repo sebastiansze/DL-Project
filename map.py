fe33616d8f36f1ea7bc7c70ba27ac1c717c7070a
Robert Hülsmann <robert@huelsi.de>
2020-05-29 03:25:12 +0200

    working on map

--- map.py
+++ map.py
@@ -1,7 +1,10 @@
 import copy
+import colorsys
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
+from matplotlib.textpath import TextPath
+from matplotlib.font_manager import FontProperties
 
 
 class Map:
@@ -40,14 +43,16 @@ class Map:
             else:
                 raise ValueError('obstacle map must have the same size as main map')
 
-        # TODO: History
-        self._hist = None
+        self._hist = np.zeros((0, agent_count, 2))
 
     def _layer_filter(self, agent=None, layer=None):
         agent = None if agent is None else str(int(agent) + 1)  # in self._layers agent number starts at one
         agent_filter = [True for _ in range(self._layers.shape[0])] if agent is None else self._layers[:, 0] == agent
         layer_filter = [True for _ in range(self._layers.shape[0])] if layer is None else self._layers[:, 1] == layer
         return np.array(agent_filter) & np.array(layer_filter)
+
+    def _add_hist(self, positions):
+        self._hist = np.concatenate([self._hist, positions], 0)
 
     def set_position(self, agent, layer, x, y):
         """
@@ -69,6 +74,10 @@ class Map:
         """
         if layer == 'n' and not self._next_step:
             raise Exception('Next position not activated in this map')
+
+        # add to history
+        if layer == 'c':
+            self._hist = np.concatenate([self._hist, np.expand_dims(positions, 0)])
 
         positions = np.concatenate([np.expand_dims(np.arange(self._agent_count), 1), positions], 1)
         ary = np.zeros((self._agent_count, self._size_x, self._size_y))
@@ -111,6 +120,14 @@ class Map:
         # other_pos = other_pos & ...
         return np.concatenate((obstacles, a_c_n_pos, other_pos))
 
+    def get_start_positions(self, agent=None):
+        if self._hist.shape[0] == 0:
+            return None
+        if agent is None:
+            return self._hist[0]
+        else:
+            return self._hist[0, agent]
+
     def plot_overview(self):
         def draw_layer(ax, layer, color):
             for x in range(self._size_x):
@@ -120,27 +137,57 @@ class Map:
                                                  facecolor=color)
                         ax.add_patch(rect)
 
-        # Create figure and axes
-        fig, ax = plt.subplots(1)
+        def draw_label(ax, x, y, text, color):
+            prop = FontProperties(family='monospace', weight='black')
+            tp = TextPath((x, y), text, prop=prop, size=1)
+            polygon = tp.to_polygons()
+            for a in polygon:
+                patch = patches.Polygon(a, facecolor=color, edgecolor='black', linewidth=1)
+                ax.add_patch(patch)
 
-        # Draw Border
-        border = patches.Rectangle((0, 0), self._size_y, self._size_x, linewidth=1, edgecolor='black', facecolor='none')
-        ax.add_patch(border)
+        # Create figure and axes
+        fig, ax = plt.subplots(1, figsize=(5, 5))
 
         # Obstacles
         obstacles = self.get_filtered_map(layer='o')[0]
-        print(obstacles)
         draw_layer(ax, obstacles, 'black')
 
         # Agents fields
-        # TODO: continue
-        # colormap = ...
-        # for agent in range(self._agent_count)
-        #     color = ...
-        #     ...
+        for agent in range(self._agent_count):
+            start_pos = self.get_start_positions(agent=agent)
+            a_c_n_pos = self._map[self._layer_filter(agent=agent)]
+
+            hue = 1.0 / self._agent_count * agent
+            color = colorsys.hsv_to_rgb(hue, 1.0, 0.7)
+            color_next = colorsys.hsv_to_rgb(hue, 0.1, 0.9)
+
+            # Plot next position
+            if self._next_step:
+                draw_layer(ax, a_c_n_pos[2], color_next)
+
+            # Plot current position
+            draw_layer(ax, a_c_n_pos[1], color)
+
+            # Plot start position
+            if start_pos is not None:
+                x = start_pos[1] + 0.2
+                y = self._size_y - start_pos[0] - 1 + 0.15
+                draw_label(ax, x, y, "S", color)
+
+            # Plot aim position
+            for y, x in zip(*np.where(a_c_n_pos[0])):
+                x = x + 0.2
+                y = self._size_y - y - 1 + 0.15
+                draw_label(ax, x, y, "E", color)
+
+        # Draw Border
+        border = patches.Rectangle((0, 0), self._size_y, self._size_x, linewidth=5, edgecolor='black',
+                                   facecolor='none')
+        ax.add_patch(border)
 
         plt.ylim(0, self._size_x)
         plt.xlim(0, self._size_y)
+        plt.axis('off')
         plt.show()
 
 
@@ -151,14 +198,18 @@ if __name__ == '__main__':
                   [0, 0, 1, 0, 0, 0],
                   [0, 0, 0, 0, 0, 0],
                   [0, 0, 0, 0, 0, 0]])
-    game_map = Map(6, 6, 2, o)
+    game_map = Map(6, 6, 2, o, next_step=True)
 
-    game_map.set_aim_positions([[5, 5],
-                                [3, 5]])
+    game_map.set_aim_positions([[0, 3],
+                                [1, 3]])
     game_map.set_current_positions([[0, 0],
-                                    [0, 5]])
+                                    [1, 0]])
+    game_map.set_current_positions([[0, 1],
+                                    [1, 1]])
+    game_map.set_next_positions([[0, 2],
+                                 [1, 2]])
 
-    # print(game_map.get_map())
+    print(game_map.get_map())
     # print(game_map.get_map_for_agent(0))
     # print(game_map.get_filtered_map(agent='2', layer='a'))
     game_map.plot_overview()
